d547db408d6d9428304ff5b83fc340612f6a8ce0
Plínio Alcântara <plinio.alcantara@dcx.ufpb.br>
2025-07-12 17:52:26 -0300

    Criando a branch repository para cliente e fatura

--- gerenciador-de-clientes/src/main/java/com/example/gerenciador_de_clientes/repository/ClienteRepository.java
+++ gerenciador-de-clientes/src/main/java/com/example/gerenciador_de_clientes/repository/ClienteRepository.java
@@ -1,4 +1,11 @@
 package com.example.gerenciador_de_clientes.repository;
 
-public class ClienteRepository {
+import com.example.gerenciador_de_clientes.model.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+
+    List<Cliente> findByStatusBloqueio(String statusBloqueio);
 }
--- gerenciador-de-clientes/src/main/java/com/example/gerenciador_de_clientes/repository/FaturaRepository.java
+++ gerenciador-de-clientes/src/main/java/com/example/gerenciador_de_clientes/repository/FaturaRepository.java
@@ -1,4 +1,12 @@
 package com.example.gerenciador_de_clientes.repository;
 
-public class FaturaRepository {
+import com.example.gerenciador_de_clientes.model.Fatura;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface FaturaRepository extends JpaRepository<Fatura, Long> {
+
+    List<Fatura> findByStatus(String status);
+    List<Fatura> findByClienteId(Long clienteId);
 }
