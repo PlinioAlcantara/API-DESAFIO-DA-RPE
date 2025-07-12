8b79d52b387049996377a0276a9978dde323c910
Plínio Alcântara <plinio.alcantara@dcx.ufpb.br>
2025-07-12 17:38:38 -0300

    Criação do model de cliente e fatura

--- gerenciador-de-clientes/src/main/java/com/example/gerenciador_de_clientes/model/Cliente.java
+++ gerenciador-de-clientes/src/main/java/com/example/gerenciador_de_clientes/model/Cliente.java
@@ -1,4 +1,79 @@
 package com.example.gerenciador_de_clientes.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String cpf;
+    private String dataNascimento;
+    private String statusBloqueio;
+    private Double limiteCredito;
+
+    @OneToMany(mappedBy = "cliente")
+    private List<Fatura> faturas;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(String dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getStatusBloqueio() {
+        return statusBloqueio;
+    }
+
+    public void setStatusBloqueio(String statusBloqueio) {
+        this.statusBloqueio = statusBloqueio;
+    }
+
+    public Double getLimiteCredito() {
+        return limiteCredito;
+    }
+
+    public void setLimiteCredito(Double limiteCredito) {
+        this.limiteCredito = limiteCredito;
+    }
+
+    public List<Fatura> getFaturas() {
+        return faturas;
+    }
+
+    public void setFaturas(List<Fatura> faturas) {
+        this.faturas = faturas;
+    }
 }
