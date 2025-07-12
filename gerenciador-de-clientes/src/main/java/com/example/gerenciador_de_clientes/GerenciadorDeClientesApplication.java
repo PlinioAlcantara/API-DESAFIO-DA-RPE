72f3d6f041558efdbaa9ddf6e5fbea49bd5bca88
Plínio Alcântara <plinio.alcantara@dcx.ufpb.br>
2025-07-12 15:03:18 -0300

    Preparando o ambiente e desenvolvendo a API Spring

---
+++ gerenciador-de-clientes/src/main/java/com/example/gerenciador_de_clientes/GerenciadorDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.example.gerenciador_de_clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciadorDeClientesApplication.class, args);
+	}
+
+}
---
+++ gerenciador-de-clientes/src/test/java/com/example/gerenciador_de_clientes/GerenciadorDeClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gerenciador_de_clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
