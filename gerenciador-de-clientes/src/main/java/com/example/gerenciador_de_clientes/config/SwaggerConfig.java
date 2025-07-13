4b20a6bd5ca411ebbfe43ebd0ceb5d90cf45d25a
Plínio Alcântara <plinio.alcantara@dcx.ufpb.br>
2025-07-13 11:43:04 -0300

    Configurando a documentação da API utilziando o Swagger

--- gerenciador-de-clientes/src/main/java/com/example/gerenciador_de_clientes/config/SwaggerConfig.java
+++ gerenciador-de-clientes/src/main/java/com/example/gerenciador_de_clientes/config/SwaggerConfig.java
@@ -1,4 +1,19 @@
 package com.example.gerenciador_de_clientes.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API - Gerenciador de Clientes")
+                        .description("Documentação da API do desafio técnico RPE")
+                        .version("1.0"));
+    }
 }
