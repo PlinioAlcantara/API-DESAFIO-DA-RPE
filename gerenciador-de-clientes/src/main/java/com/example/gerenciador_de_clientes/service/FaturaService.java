6b7002285946828b563b9a558921c9a2daa0a257
Plínio Alcântara <plinio.alcantara@dcx.ufpb.br>
2025-07-13 22:23:47 -0300

    Realizando modificações para atualizar o valor do limite de crédito

--- gerenciador-de-clientes/src/main/java/com/example/gerenciador_de_clientes/service/FaturaService.java
+++ gerenciador-de-clientes/src/main/java/com/example/gerenciador_de_clientes/service/FaturaService.java
@@ -1,6 +1,8 @@
 package com.example.gerenciador_de_clientes.service;
 
+import com.example.gerenciador_de_clientes.model.Cliente;
 import com.example.gerenciador_de_clientes.model.Fatura;
+import com.example.gerenciador_de_clientes.repository.ClienteRepository;
 import com.example.gerenciador_de_clientes.repository.FaturaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,17 +15,31 @@ public class FaturaService {
     @Autowired
     private FaturaRepository faturaRepository;
 
+    @Autowired
+    private ClienteRepository clienteRepository;
+
     public List<Fatura> listarFaturas(Long clienteId) {
         return faturaRepository.findByClienteId(clienteId);
     }
 
     public Fatura registrarPagamento(Long faturaId, Fatura fatura) {
         Fatura faturaExistente = faturaRepository.findById(faturaId).orElse(null);
-        if (faturaExistente != null) {
+
+        if (faturaExistente != null && !faturaExistente.getStatus().equals("P")) {
             faturaExistente.setStatus("P");
             faturaExistente.setDataPagamento(fatura.getDataPagamento());
+
+
+            Cliente cliente = faturaExistente.getCliente();
+            if (cliente != null) {
+                double novoLimite = cliente.getLimiteCredito() + faturaExistente.getValor();
+                cliente.setLimiteCredito(novoLimite);
+                clienteRepository.save(cliente);
+            }
+
             return faturaRepository.save(faturaExistente);
         }
+
         return null;
     }
 
